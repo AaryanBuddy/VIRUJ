8eb604e754ca79e2581f12c3a9494e8f4783da13
muskaan-11 <alagmuskaan@gmail.com>
2025-03-28 21:26:23 +0530

    commit

--- app/src/main/java/com/example/viruj/brief3.kt
+++ app/src/main/java/com/example/viruj/brief3.kt
@@ -13,12 +13,7 @@ class brief3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_brief3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val nextButton : Button= findViewById(R.id.buttonbrief1)
+        val nextButton : Button= findViewById(R.id.buttonbrief3)
         nextButton.setOnClickListener {
             val intent = Intent(this , Login::class.java)
             startActivity(intent)
